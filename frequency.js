c7defcfac9719d9300fcc123a77861cacaf15b5b
Suseendhiran <58186071+Suseendhiran@users.noreply.github.com>
2025-04-02 14:59:54 +0530

    Update frequency.js

--- frequency.js
+++ frequency.js
@@ -1,1 +1,20 @@
+//O(n**2)
 
+function sameFrequency(arr1, arr2){
+    if(arr1.length !== arr2.length){
+        return "Not same"
+    }
+    for (let i in arr1){
+        let squaredIndex = arr2.indexOf(arr1[i]**2)
+        if(!arr2.includes(arr1[i]**2)){
+            return "Not same"
+        }
+        arr2.splice(squaredIndex,1)
+        console.log("sss",i,squaredIndex)
+    }
+    return "Same"
+}
+
+const output = sameFrequency([1,2,3,4,8],[16,1,4,9,45])
+
+console.log("output",output)
