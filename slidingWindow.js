22535a49ec59fef6411eea28f7f6c07e3ff20f9f
Suseendhiran <58186071+Suseendhiran@users.noreply.github.com>
2025-04-20 20:14:30 +0530

    Update slidingWindow.js

--- slidingWindow.js
+++ slidingWindow.js
@@ -1,4 +1,4 @@
-//my solution/ maximum sum of n consecutive elements in an array 
+//my solution/ maximum sum of n consecutive elements in an array O(n sqaure)
 
 function maxSubArraySum(arr,num){
     //[1,2,5,2,8,1,5], 4
@@ -19,3 +19,22 @@ function maxSubArraySum(arr,num){
     return output
 }
 console.log(maxSubArraySum([], 4))
+
+//sliding window pattern O(n), sum first num elements, add next element, subtract prev element.
+function maxSubArraySum(arr,num){
+    //[1,2,5,2,8,1,5], 3
+    if(!arr.length) return null;
+    let maxSum = 0;
+    let tempSum = 0;
+    for(let i=0;i<num; i++){
+        maxSum+=arr[i]
+    }
+    tempSum = maxSum
+    for(let i=num; i<arr.length; i++){
+        tempSum = tempSum + arr[i] - arr[i-num]
+        maxSum = Math.max(maxSum,tempSum)
+    }
+    console.log(maxSum)
+    return maxSum
+}
+console.log(maxSubArraySum([2,6,9,2,1,8,5,6,3], 3))
