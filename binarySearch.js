ef80e08a69baf34204d24c5affc3a4eaf1b187e9
Suseendhiran <58186071+Suseendhiran@users.noreply.github.com>
2025-05-18 23:03:16 +0530

    Update binarySearch.js

--- binarySearch.js
+++ binarySearch.js
@@ -24,3 +24,23 @@ function binarySearch(arr,val){
     return -1
   // add whatever parameters you deem necessary - good luck!
 }
+
+//course's solution
+function binarySearch(arr,val){
+    let leftPointer = 0;
+    let rightPointer = arr.length - 1
+    let middlePointer = Math.floor(arr.length/2)
+    while(arr[middlePointer]!== val && leftPointer<=rightPointer){
+        if(val>arr[middlePointer]){
+            leftPointer = middlePointer+1
+        }
+        else{
+            rightPointer = middlePointer - 1
+        }
+        middlePointer = Math.ceil((leftPointer+rightPointer)/2)
+    }
+    return arr[middlePointer] === val ? middlePointer : -1
+  // add whatever parameters you deem necessary - good luck!
+}
+
+console.log(binarySearch([1,2,3,4,5],10)  )
