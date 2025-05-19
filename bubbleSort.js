fa376593d3c2d892647894ef6de49261ad4ea38d
Suseendhiran <58186071+Suseendhiran@users.noreply.github.com>
2025-05-19 19:07:42 +0530

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -13,5 +13,19 @@ function bubbleSort(arr){
     return arr
 }
 
+//reduce no of comparison operation, once the element bubble sorted, we dont need to make comparison for that value as it is the largest in array, so iterating from array end
+function bubbleSort(arr){
+    for(let i=arr.length-1;i>0;i--){
+        for(j=0;j<i;j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j+1];
+                arr[j+1] = arr[j] 
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
+}
+
 
 console.log(bubbleSort([10,5,1,9,8,3]))
