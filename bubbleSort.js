7baedbafefe93d9b9b0c14877750cadfa56d0bf2
Suseendhiran <58186071+Suseendhiran@users.noreply.github.com>
2025-05-19 19:00:41 +0530

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,1 +1,17 @@
 
+
+function bubbleSort(arr){
+    for(let i=0;i<arr.length;i++){
+        for(j=0;j<arr.length;j++){
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j+1];
+                arr[j+1] = arr[j] 
+                arr[j] = temp
+            }
+        }
+    }
+    return arr
+}
+
+
+console.log(bubbleSort([10,5,1,9,8,3]))
