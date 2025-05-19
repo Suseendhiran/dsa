1e06dd088c481a0553817b22a549224c9cf3d499
Suseendhiran <58186071+Suseendhiran@users.noreply.github.com>
2025-05-19 22:45:44 +0530

    Update bubbleSort.js

--- bubbleSort.js
+++ bubbleSort.js
@@ -26,6 +26,25 @@ function bubbleSort(arr){
     }
     return arr
 }
+//in the above function even if the array is sorted, it keeps on iterating until the i loop condition is getting satisified, so creating noSwap variable, if noSwap break outer loop
+
+function bubbleSort(arr){
+    var noSwap;
+    for(let i=arr.length-1;i>0;i--){
+        noSwap = true
+        for(j=0;j<i;j++){
+            console.log(arr[j],arr[j+1])
+            if(arr[j]>arr[j+1]){
+                let temp = arr[j+1];
+                arr[j+1] = arr[j] 
+                arr[j] = temp
+                noSwap = false
+            }
+        }
+        if(noSwap) break;
+    }
+    return arr
+}
 
 
 console.log(bubbleSort([10,5,1,9,8,3]))
